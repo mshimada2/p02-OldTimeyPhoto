00ae7b5bb29d97229d1071d7ba38e766c2a9c051
mshimada <mshimada2@mail.csuchico.edu>
2017-10-02 17:04:29 -0700

    Make functional decomposition

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string> 
+
+using namespace std;
+
+
+int main(){
+
+
+//ask user for bitmap file 
+//use cout and cin for user input
+
+
+
+//convert image to greyscale to look old timey 
+//use vectors and or matrices 
+
+
+//save file 
+
+
+
+
+
+return 0;
+
+}
