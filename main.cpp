f9963c69ad0d641d20f5eaff15ae1bcc5e68994a
mshimada <mshimada2@mail.csuchico.edu>
2017-10-04 19:35:34 -0700

    Programmed it so it can tell you the width and height of the bitmap image in pixels.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 #include<string> 
+#include"bitmap.h"
 
 using namespace std;
 
 
 int main(){
 
-
-//ask user for bitmap file 
+//ask user for bitmap file
 //use cout and cin for user input
 
 
 
+Bitmap img;
+vector <vector <Pixel> > bmp; 
+  
+ 
+
 //convert image to greyscale to look old timey 
-//use vectors and or matrices 
+img.open("machupicchu.bmp");
+bmp = img.toPixelMatrix(); 
 
-
-//save file 
-
-
-
+cout<<endl<<"Machu Picchu has been uploaded! The width is "<<bmp[0].size()<<". The height is "<<bmp.size()<<". ";
 
 
 return 0;
