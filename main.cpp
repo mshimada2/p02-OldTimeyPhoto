63269247d1934790969e62f7bde7a38670f16d39
mshimada <mshimada2@mail.csuchico.edu>
2017-10-11 20:44:40 -0700

    programmed it so you can save the old timey image

--- main.cpp
+++ main.cpp
@@ -14,14 +14,41 @@ int main(){
 
 Bitmap img;
 vector <vector <Pixel> > bmp; 
-  
+Pixel rgb;  
  
 
 //convert image to greyscale to look old timey 
 img.open("machupicchu.bmp");
-bmp = img.toPixelMatrix(); 
+bmp = img.toPixelMatrix();
+
+bool validBmp = img.isImage();
+
+if(validBmp == true ){
+
+    bmp = img.toPixelMatrix();
+
+
+
+
+    rgb = bmp[0][0];
+    rgb.red = 230; 
+    rgb.green = 230;
+    rgb.blue = 230;
+
+
+
+    bmp[0][0] = rgb;
+    img.fromPixelMatrix(bmp);
+    img.save("machupicchu.bmp");
+
+    }
+
+
 
 cout<<endl<<"Machu Picchu has been uploaded! The width is "<<bmp[0].size()<<". The height is "<<bmp.size()<<". ";
+
+
+
 
 
 return 0;
